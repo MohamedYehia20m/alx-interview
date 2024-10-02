d3cca590fe6c7d0ae49e477bddc579b5c351599a
MohamedYehia20m <medoyehia2001@gmail.com>
2024-10-02 12:00:02 +0300

    adhere to pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,20 +3,22 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
-        """Create a function def pascal_triangle(n): that returns a list of lists
-        of integers representing the Pascal’s triangle of n
-        """
-        triangle = [[1]]
+    """
+    Create a function def pascal_triangle(n): that returns
+    a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = [[1]]
 
-        #looping rows from 1 to n -1
-        for i in range(1,n):
-                row = []
+    # looping rows from 1 to n -1
+    for i in range(1, n):
+        row = []
 
-                row.append(1) #start
-                # looping row cells to fill it with values
-                for j in range(i - 1): # for row no.2 we fill a single cell with index 0.
-                        row.append(triangle[i-1][j] + triangle[i-1][j + 1])
-                row.append(1) #end
-                triangle.append(row)
-        return triangle
+        row.append(1)  # start
+        # looping row cells to fill it with values
+        for j in range(i - 1):  # for row no.2 we fill one cell with index 0.
+            row.append(triangle[i-1][j] + triangle[i-1][j + 1])
+        row.append(1)  # end
+    triangle.append(row)
+    return triangle
