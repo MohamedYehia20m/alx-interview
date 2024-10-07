8c58aed39638013ee50893d8f7e953b36ee7ea8f
MohamedYehia20m <medoyehia2001@gmail.com>
2024-10-07 12:57:22 +0300

    init 0x01-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    0-lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+
+    """
+    distinct_keys = set(boxes[0])
+    keys = list(distinct_keys)
+    n = len(boxes)
+
+    for i in range(1,n):
+        if len(distinct_keys) == n - 1:
+            return True
+        if i > len(distinct_keys):
+            return False
+        distinct_keys.update(boxes[keys[i - 1]])
+        keys = list(distinct_keys)
+
+    return False
+
