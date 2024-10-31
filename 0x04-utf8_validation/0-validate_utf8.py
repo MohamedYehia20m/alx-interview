75dff5f57f301890f7bcbe8f8f3460f08afed15f
MohamedYehia20m <medoyehia2001@gmail.com>
2024-10-31 12:58:09 +0300

    adhere to requirement

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
+"""
+Module for validating UTF-8 encoding.
+"""
 
 
 def validUTF8(data):
+    """
+    Validate if the given data is a valid UTF-8 encoding.
+
+    Args:
+        data (list): List of integers representing bytes.
+
+    Returns:
+        bool: True if data is valid UTF-8 encoding, else False.
+    """
     num_bytes = 0  # Number of bytes to expect
+
     for byte in data:
         # Get the last 8 bits of the integer
         byte &= 0xFF
 
         # Determine the number of bytes in the character
         if num_bytes == 0:
-            # Count leading 1s to determine the No. of bytes in this character
+            # Count leading 1s to determine the no. of bytes in this character
             if (byte >> 7) == 0b0:  # 1-byte character
                 num_bytes = 0
             elif (byte >> 5) == 0b110:  # 2-byte character
