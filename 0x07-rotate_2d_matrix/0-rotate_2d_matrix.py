503a72f3e196cf49e73981c3a33855f2686c2f1c
MohamedYehia20m <medoyehia2001@gmail.com>
2024-11-21 10:37:18 +0200

    add module documentation

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,4 +6,13 @@
     rotate n x n 2D matrix 90 degrees clockwise.
 """
 def rotate_2d_matrix(matrix):
+    """
+    Rotates a square 2D matrix in place 90 degrees clockwise.
+
+    Args:
+        matrix (list[list[int]]): The n x n 2D matrix to rotate.
+
+    Returns:
+        None: The matrix is modified in place.
+    """
     matrix[:] = zip(*matrix[::-1])
